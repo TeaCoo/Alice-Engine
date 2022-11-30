f5da639c3a241ee66ec90bfa4fc648a4cdf6502f
Xiaoyuan Yang <54407283+TeaCoo@users.noreply.github.com>
2022-11-30 20:03:36 +0800

    Update setup.bat
    add git command in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,6 @@
+git submodule add https://github.com/TeaCoo/glfw.git Alice/extern/GLFW
+git submodule add https://github.com/TeaCoo/imgui.git Alice/extern/imgui
+git submodule add https://github.com/gabime/spdlog.git Alice/extern/spdlog
+
 call extern\bin\premake\premake5.exe vs2019
 PAUSE
