e3d9f7bd9da892de4349e70a8cabe87bb740dd90
Xiaoyuan Yang <54407283+TeaCoo@users.noreply.github.com>
2022-11-30 20:08:47 +0800

    Update setup.bat
    remove the line in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,2 @@
-git submodule add https://github.com/TeaCoo/glfw.git Alice/extern/GLFW
-git submodule add https://github.com/TeaCoo/imgui.git Alice/extern/imgui
-git submodule add https://github.com/gabime/spdlog.git Alice/extern/spdlog
-
 call extern\bin\premake\premake5.exe vs2019
 PAUSE
